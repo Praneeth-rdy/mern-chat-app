a8b4a5e2188dc711d34c3d92e3290ba7f54c03f3
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-06-10 19:51:34 +0530

    Added 404.html to public folder to redirect all the sub urls of the application to index.js instead of a 404 page and changed the router from hashrouter into BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 // Importing Screens
@@ -12,7 +12,7 @@ import HomeScreen from './components/screens/HomeScreen';
 
 function App() {
   return (
-    <Router basename="mern-chat-app">
+    <Router basename="/mern-chat-app">
       <div className="app">
         <Switch>
           <Route exact path="/" component={HomeScreen} />
